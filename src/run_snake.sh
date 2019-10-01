9ef0d466ad6722de786b474cfcb6e72d7db32dc3
Dean Pettinga <deanpett@gmail.com>
2019-10-01 17:07:21 -0400

    added singularity 3.4 implementation into run_snake.sh

--- src/run_snake.sh
+++ src/run_snake.sh
@@ -1,24 +1,31 @@
 #PBS -l walltime=100:00:00
 #PBS -l mem=8gb
-#PBS -l nodes=1:ppn=1
+#PBS -l nodes=node065:ppn=1
 #PBS -m ae
 #PBS -N rnaseq_workflow
 #PBS -o logs/rnaseq_workflow.o
 #PBS -e logs/rnaseq_workflow.e
 
 cd ${PBS_O_WORKDIR}
+
+# export Singularity 3.4.0-1 installed on node065 to $PATH.
+# snakemake should now automatically call Singularity 3.4.0-1
+PATH=/usr/local/bin/:$PATH
+
+conda activate snakemake
 # save DAG job file with time stamp
-conda activate /secondary/projects/bbc/tools/workflow_tools/miniconda3/envs/snakemake
 TIME=$(date "+%Y-%m-%d_%H.%M.%S")
 snakemake --use-conda -n > logs/runs/rnaseq-workflow_${TIME}.txt
 snakemake --dag | dot -Tpng > logs/runs/rnaseq-workflow_${TIME}.png
 
+# run snakemake
 snakemake \
 -s Snakefile \
 -j 48 \
 --cluster-config src/cluster.yaml \
 --cluster 'qsub -q {cluster.qname} -l nodes={cluster.nodes}:ppn={cluster.ppn} -l mem={cluster.mem} -l walltime={cluster.time} -m ea -o error_files/ -e error_files/' \
 --use-conda \
+--use-singularity
 
 # generate report
 #snakemake \
