a80a90e78dca0f41da4e516ca741f8549eaee0ed
genomics-kl <kin.lau@vai.org>
2020-10-28 14:51:21 -0400

    Bug fix for mergeLanesAndRename.R for cases where there are multiple fastq files (units) per sample.

--- bin/mergeLanesAndRename.R
+++ bin/mergeLanesAndRename.R
@@ -15,14 +15,14 @@ samp = snakemake@output[[1]] %>% gsub("raw_data/","",.) %>% gsub("(-.{2}).fastq.
 samp_units <- units %>% dplyr::filter(sample==samp)
 
 # check that fastq files are gzipped (have .gz suffix)
-fq1_ends_with_gz <- samp_units$fq1 %>% stringr::str_detect(".gz$")
+fq1_ends_with_gz <- all(samp_units$fq1 %>% stringr::str_detect(".gz$"))
 if(!fq1_ends_with_gz){
     stop(paste0("fq1 for ", samp, " not a gzipped file"))
 }
 
 # check fq2 similarly only if the config file indicates PE reads
 if (config$PE_or_SE=="PE"){
-    fq2_ends_with_gz <- samp_units$fq2 %>% stringr::str_detect(".gz$")
+    fq2_ends_with_gz <- all(samp_units$fq2 %>% stringr::str_detect(".gz$"))
     if(!fq2_ends_with_gz){
         stop(paste0("fq2 for ", samp, " not a gzipped file"))
     }
@@ -36,12 +36,14 @@ if (config$PE_or_SE=="SE"){
   print(paste("merging SE reads for:", samp))
 
 
-  if (file.exists(paste0("raw_data/", samp_units$fq1))){
+  if (all(file.exists(paste0("raw_data/", samp_units$fq1)))){
     # merge SE reads
     bash_call <- ifelse(nrow(samp_units) > 1, 
                         paste0("cat ", gsub(toString(paste0("raw_data/", samp_units$fq1)), pattern=",", replacement = ""), " > "), 
                         paste0("ln -s ", samp_units$fq1, " "))
-    system(paste0(bash_call, "raw_data/", samp, "-SE.fastq.gz"))
+    bash_call <- paste0(bash_call, "raw_data/", samp, "-SE.fastq.gz")
+    print(bash_call)
+    system(bash_call)
     print("R1 units merged")
     save.image(file=paste0("logs/mergeLanesAndRename/mergeLanesAndRename_SE-",samp,".RData"))
   } else {
@@ -54,21 +56,25 @@ if (config$PE_or_SE=="SE"){
   print(paste("merging PE reads for:", samp))
 
 
-  if (file.exists(paste0("raw_data/", samp_units$fq1))){
+  if (all(file.exists(paste0("raw_data/", samp_units$fq1)))){
     bash_call <- ifelse(nrow(samp_units) > 1, 
                         paste0("cat ", gsub(toString(paste0("raw_data/", samp_units$fq1)), pattern=",", replacement = ""), " > "), 
                         paste0("ln -s ", samp_units$fq1, " "))
-    system(paste0(bash_call, "raw_data/", samp, "-R1.fastq.gz"))
+    bash_call <- paste0(bash_call, "raw_data/", samp, "-R1.fastq.gz")
+    print(bash_call)
+    system(bash_call)
     print("R1 units merged")
   } else {
     stop(paste("Error in mergeLanesAndRename.R: fq1 file for", samp, "listed in bin/units.tsv not present in raw_data/."))
   }
   # merge R2 reads
-  if (file.exists(paste0("raw_data/", samp_units$fq2))){
+  if (all(file.exists(paste0("raw_data/", samp_units$fq2)))){
     bash_call <- ifelse(nrow(samp_units) > 1, 
                         paste0("cat ", gsub(toString(paste0("raw_data/", samp_units$fq2)), pattern=",", replacement = ""), " > "), 
                         paste0("ln -s ", samp_units$fq2, " "))
-    system(paste0(bash_call, "raw_data/", samp, "-R2.fastq.gz"))
+    bash_call <- paste0(bash_call, "raw_data/", samp, "-R2.fastq.gz")
+    print(bash_call)
+    system(bash_call)
     print("R2 units merged")
     save.image(file=paste0("logs/mergeLanesAndRename/mergeLanesAndRename_PE-",samp,".RData"))
   } else {
