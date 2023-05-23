12a4140a1cae30fce33a3398a588c758c9b56e52
genomics-kl <kin.lau@vai.org>
2023-05-23 13:57:34 -0400

    Workflow running on SLURM

--- bin/run_snake.sh
+++ bin/run_snake.sh
@@ -1,45 +1,42 @@
-#PBS -l walltime=100:00:00
-#PBS -l mem=8gb
-#PBS -l nodes=node065
-#PBS -m ae
-#PBS -N rnaseq_workflow
-#PBS -o logs/rnaseq_workflow.o
-#PBS -e logs/rnaseq_workflow.e
+#!/bin/bash
+#SBATCH --export=NONE
+#SBATCH -J rnaseq_workflow
+#SBATCH -o rnaseq_workflow.o
+#SBATCH -e rnaseq_workflow.e
+#SBATCH --ntasks 1
+#SBATCH --time 24:00:00
+#SBATCH --mem=8G
 
-cd ${PBS_O_WORKDIR}
-# export Singularity 3.4.0-1 installed on node065 to $PATH.
-# snakemake should now automatically call Singularity 3.4.0-1
-#PATH=/usr/local/bin/:$PATH
+cd $SLURM_SUBMIT_DIR
 
-module load bbc/snakemake/snakemake-5.28.0
+snakemake_module="bbc2/snakemake/snakemake-7.25.0"
 
-# specify which conda installation to use
-conda_setup='/primary/vari/software/BBC/miniconda_bare/etc/profile.d/conda.sh'
+module load $snakemake_module
 
-# this make 'conda' callable and allows conda environments to be created.
-source $conda_setup
-
-# save DAG job file with time stamp
-TIME=$(date "+%Y-%m-%d_%H.%M.%S")
-
-# make logs dir if it does not exist already. Without this, logs/ is automatically generate only after the first run of the pipeline
-logs_dir="logs/runs"
+# make logs dir if it does not exist already. 
+logs_dir="logs/"
 [[ -d $logs_dir ]] || mkdir -p $logs_dir
 
-snakemake --use-envmodules -n > logs/runs/workflow_${TIME}.txt
-snakemake --dag | dot -Tpng > logs/runs/workflow_${TIME}.png
 
+echo "Start snakemake workflow." >&1                   
+echo "Start snakemake workflow." >&2     
 
 snakemake \
 -p \
+--latency-wait 20 \
+--snakefile 'Snakefile' \
 --use-envmodules \
---use-conda \
 --jobs 100 \
---cluster "qsub \
--q bbc \
--V \
--l nodes=1:ppn={threads} \
--l mem={resources.mem_gb}gb \
--l walltime=100:00:00 \
--o logs/runs/ \
--e logs/runs/"
+--cluster "ssh ${SLURM_JOB_USER}@access.hpc.vai.org 'module load $snakemake_module; cd $SLURM_SUBMIT_DIR; mkdir -p logs/{rule}; sbatch \
+-p ${SLURM_JOB_PARTITION} \
+--export=ALL \
+--ntasks {threads} \
+--mem={resources.mem_gb}G \
+-t 48:00:00 \
+-o logs/{rule}/{resources.log_prefix}.o \
+-e logs/{rule}/{resources.log_prefix}.e'" # SLURM hangs if output dir does not exist, so we create it before running sbatch on the snakemake jobs.
+#--slurm \
+#--default-resources slurm_account=${SLURM_JOB_USER} slurm_partition=${SLURM_JOB_PARTITION}
+
+echo "snakemake workflow done." >&1                   
+echo "snakemake workflow done." >&2                
