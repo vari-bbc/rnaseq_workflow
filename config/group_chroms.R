bb55795d041d51d6065d61fa4bee8329d42834a1
kew24 <kaitlyn.westra@vai.org>
2024-12-16 11:02:05 -0500

    group_chroms.R can handle quick_ref in config

--- config/group_chroms.R
+++ config/group_chroms.R
@@ -5,7 +5,29 @@ suppressPackageStartupMessages(library(magrittr))
 suppressPackageStartupMessages(library(Rsamtools))
 
 # params from workflow
-ref_fasta <- read_yaml("config.yaml")$ref$sequence
+# check if quick_ref is specified
+quick_ref <- read_yaml("config.yaml")$quick_ref
+if (is.null(quick_ref$species_name)) {
+    cat("Using the index files manually specified in the config file.\n")
+    ref_fasta <- read_yaml("config.yaml")$ref$sequence
+} else {
+    cat("Using quick_ref instead of manually indicated index files.\n")
+    if (is.null(quick_ref$ref_genome_version)) {
+        cat("Version number not specified. Will use the latest version of the BBC-curated reference files.\n")
+        quick_ref$ref_genome_version <- "latest"
+    }
+    ref_fasta <- paste0("/varidata/research/projects/bbc/versioned_references/", 
+                        quick_ref$ref_genome_version, 
+                        "/data/", 
+                        quick_ref$species_name, 
+                        "/sequence/",
+                        quick_ref$species_name,
+                        ".fa")
+    if (!file.exists(ref_fasta)) {
+        stop(paste("The quick_ref reference fasta file does not exist:", ref_fasta))
+    }
+}
+
 outfile <- "grouped_contigs.tsv"
 
 # make GenomicRanges from the genome
