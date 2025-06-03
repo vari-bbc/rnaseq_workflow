2c4305e79636b545ccc762179c3aea6122bd9d82
genomics-kl <kin.lau@vai.org>
2025-06-03 08:29:33 -0400

    Try to get renv to install only released Bioconductor packages, not devel, and from same BioC version.

--- workflow/scripts/install_renv_pkges.R
+++ workflow/scripts/install_renv_pkges.R
@@ -5,13 +5,15 @@ pkges_file <- args[1]
 # https://github.com/rstudio/renv/issues/81#issuecomment-497131224
 options(repos = BiocManager::repositories()) 
 
+# Get list of packages available on BioConductor
+bioc_repos <- BiocManager::repositories()
+avail_bioc <- available.packages(repos=bioc_repos[grepl("BioC", names(bioc_repos))])
+
 message("Initiating renv...")
-renv::init()
+renv::init(bioconductor=TRUE)
 
-renv::settings$bioconductor.version(BiocManager::version())
-
-message("Updating packages in renv library...")
-renv::update()
+# message("Updating packages in renv library...")
+# renv::update()
 
 message("Double check that all dependencies can be loaded.")
 
@@ -28,7 +30,14 @@ for (pkg in pkges){
             message("Removed verion number from ", pkg, " to obtain ", req_pkg, ".")
         }
         if(!suppressPackageStartupMessages(require(req_pkg, character.only=TRUE, quietly = TRUE, warn.conflicts = FALSE))){
-            message("Unable to load ", req_pkg, ". Trying to install...")
+
+            if (pkg %in% avail_bioc[, "Package"]){
+                # adding "bioc::" as this has been reported to help prevent installing devel BioC packages and keep packages on the same BioC release. See https://github.com/rstudio/renv/issues/244
+                # see also https://rstudio.github.io/renv/reference/install.html for syntax for 'packages' argument
+                pkg <- paste0("bioc::", pkg)
+            }
+
+            message("Unable to load ", req_pkg, ". Trying to install ", pkg, " ...")
             renv::install(pkg)
 		}
 }
